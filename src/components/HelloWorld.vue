65d9b4e709a3e97f5812f96f7e506f664d7d4dc7
Nick Lapitsky <nick.lapitsky@cinarra.com>
2020-05-03 22:59:47 +0300

    Added result dialog

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,45 @@
 <template>
   <v-app id="inspire">
+    <v-dialog
+      v-model="isResultShown"
+      width="500"
+    >
+      <template v-slot:activator="{ on }">
+        <v-btn
+          color="red lighten-2"
+          dark
+          v-on="on"
+        >
+          Click Me
+        </v-btn>
+      </template>
+
+      <v-card>
+        <v-card-title
+          class="headline grey lighten-2"
+          primary-title
+        >
+          Privacy Policy
+        </v-card-title>
+
+        <v-card-text>
+          {{this.isSuccessful ? 'Win': 'Fail'}}
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary"
+            text
+            @click="hideResult"
+          >
+            OK
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
     <v-content>
       <v-container class="fill-height" fluid>
         <v-row align="center" justify="center">
@@ -11,17 +51,7 @@
                     background-color="deep-purple accent-4"
                     class="elevation-2"
                     dark
-                    :centered="centered"
-                    :grow="grow"
-                    :vertical="vertical"
-                    :right="right"
-                    :prev-icon="
-                      prevIcon ? 'mdi-arrow-left-bold-box-outline' : undefined
-                    "
-                    :next-icon="
-                      nextIcon ? 'mdi-arrow-right-bold-box-outline' : undefined
-                    "
-                    :icons-and-text="icons"
+
                   >
                     <v-tabs-slider></v-tabs-slider>
 
@@ -32,17 +62,18 @@
                     >
                       {{ name }}
                     </v-tab>
-                    <v-tab-item
-                      :key="0"
-                      :value="'tab-' + 0"
-                    >
-                      <SalaryForm prompt="Minimum salary" event-id="employee"></SalaryForm>
+                    <v-tab-item :key="0" :value="'tab-' + 0">
+                      <SalaryForm
+                        prompt="Maximum salary"
+                        @confirmed="setOfferedSalary"
+                      ></SalaryForm>
+
                     </v-tab-item>
-                    <v-tab-item
-                      :key="1"
-                      :value="'tab-' + 1"
-                    >
-                      <SalaryForm prompt="Maximum salary" event-id="employer"></SalaryForm>
+                    <v-tab-item :key="1" :value="'tab-' + 1">
+                      <SalaryForm
+                        prompt="Minimum salary"
+                        @confirmed="setDemandedSalary"
+                      ></SalaryForm>
                     </v-tab-item>
                   </v-tabs>
                 </v-form>
@@ -56,53 +87,54 @@
 </template>
 
 <script lang="ts">
-    import {Component, Vue} from "vue-property-decorator";
-    import Vuetify from "vuetify";
-    import SalaryForm from "@/components/SalaryForm.vue";
+import { Component, Vue } from "vue-property-decorator";
+import Vuetify from "vuetify";
+import SalaryForm from "@/components/SalaryForm.vue";
 
-    @Component({
-        components: {SalaryForm}
-    })
-    export default class SalaryNegotiator extends Vue {
+@Component({
+  components: { SalaryForm }
+})
+export default class SalaryNegotiator extends Vue {
+  private _offeredSalary = -1;
+  private _demandedSalary = -1;
+  private isSuccessful = false;
+  isResultShown = false;
 
-        private _offeredSalary = -1;
-        private _demandedSalary = -1;
-        get demandedSalary(): number {
-            return this._demandedSalary;
-        }
+  vuetify = new Vuetify();
+  tabs = ["Employer", "Employee"];
 
-        set demandedSalary(value: number) {
-            this._demandedSalary = value;
-        }
+  setOfferedSalary(salary: number): void {
+    this._offeredSalary = salary;
+    console.log("offered salary = " + salary);
+    this.showResult();
 
-        get offeredSalary(): number {
-            return this._offeredSalary;
-        }
+  }
 
-        set offeredSalary(value: number) {
-            this._offeredSalary = value;
-        }
+  setDemandedSalary(salary: number): void {
+    this._demandedSalary = salary;
+    console.log("demanded salary = " + salary);
+    this.showResult();
+  }
 
-        vuetify = new Vuetify();
 
-        text =
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
-        icons = false;
-        centered = false;
-        grow = false;
-        vertical = false;
-        prevIcon = false;
-        nextIcon = false;
-        right = false;
-        tabs = ["Employer", "Employee"];
+  showResult(): void {
+      if (!(this._offeredSalary > 0 && this._demandedSalary>0)) {
+          console.log('salaries not set')
+          return;
+      }
 
-        setOfferedSalary(salary: number): void {
-            this._offeredSalary = salary;
-        }
+      if (this._offeredSalary > this._demandedSalary) {
+          this.isSuccessful = true;
+          console.log('success')
+      } else {
+          this.isSuccessful = false;
+          console.log('failure')
+      }
+      this.isResultShown = true;
+  }
 
-        setDemandedSalary(salary: number): void {
-            this._demandedSalary = salary;
-        }
-    }
-
+  hideResult(): void {
+      this.isResultShown = false;
+  }
+}
 </script>

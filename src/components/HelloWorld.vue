615a95cc23c0d5f1a6daf11e199b02178f97901f
Nick Lapitsky <nick.lapitsky@cinarra.com>
2020-05-03 22:24:15 +0300

    Move tab content to components

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,18 +5,6 @@
         <v-row align="center" justify="center">
           <v-col cols="12" sm="8" md="4">
             <v-card class="elevation-12">
-              <v-toolbar color="primary" dark flat>
-                <v-toolbar-title>Login form</v-toolbar-title>
-                <v-spacer />
-                <v-tooltip bottom>
-                  <template v-slot:activator="{ on }">
-                    <v-btn :href="source" icon large target="_blank" v-on="on">
-                      <v-icon>mdi-code-tags</v-icon>
-                    </v-btn>
-                  </template>
-                  <span>Source</span>
-                </v-tooltip>
-              </v-toolbar>
               <v-card-text>
                 <v-form>
                   <v-tabs
@@ -43,38 +31,22 @@
                       :href="`#tab-${i}`"
                     >
                       {{ name }}
-                      <v-icon v-if="icons">mdi-phone</v-icon>
                     </v-tab>
-
                     <v-tab-item
                       :key="0"
                       :value="'tab-' + 0"
                     >
-                      <v-card flat tile>
-                        <v-text-field
-                          label="Minimum salary"
-                          hide-details="auto"
-                        ></v-text-field>
-                      </v-card>
+                      <SalaryForm prompt="Minimum salary" event-id="employee"></SalaryForm>
                     </v-tab-item>
                     <v-tab-item
                       :key="1"
                       :value="'tab-' + 1"
                     >
-                      <v-card flat tile>
-                        <v-text-field
-                          label="Maximum salary"
-                          hide-details="auto"
-                        ></v-text-field>
-                      </v-card>
+                      <SalaryForm prompt="Maximum salary" event-id="employer"></SalaryForm>
                     </v-tab-item>
                   </v-tabs>
                 </v-form>
               </v-card-text>
-              <v-card-actions>
-                <v-spacer />
-                <v-btn color="primary">Login</v-btn>
-              </v-card-actions>
             </v-card>
           </v-col>
         </v-row>
@@ -84,22 +56,53 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import Vuetify from "vuetify";
+    import {Component, Vue} from "vue-property-decorator";
+    import Vuetify from "vuetify";
+    import SalaryForm from "@/components/SalaryForm.vue";
 
-@Component
-export default class SalaryNegotiator extends Vue {
-  vuetify = new Vuetify();
-  text =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
-  icons = false;
-  centered = false;
-  grow = false;
-  vertical = false;
-  prevIcon = false;
-  nextIcon = false;
-  right = false;
-  tabs = ["Employer", "Employee"];
-}
+    @Component({
+        components: {SalaryForm}
+    })
+    export default class SalaryNegotiator extends Vue {
+
+        private _offeredSalary = -1;
+        private _demandedSalary = -1;
+        get demandedSalary(): number {
+            return this._demandedSalary;
+        }
+
+        set demandedSalary(value: number) {
+            this._demandedSalary = value;
+        }
+
+        get offeredSalary(): number {
+            return this._offeredSalary;
+        }
+
+        set offeredSalary(value: number) {
+            this._offeredSalary = value;
+        }
+
+        vuetify = new Vuetify();
+
+        text =
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+        icons = false;
+        centered = false;
+        grow = false;
+        vertical = false;
+        prevIcon = false;
+        nextIcon = false;
+        right = false;
+        tabs = ["Employer", "Employee"];
+
+        setOfferedSalary(salary: number): void {
+            this._offeredSalary = salary;
+        }
+
+        setDemandedSalary(salary: number): void {
+            this._demandedSalary = salary;
+        }
+    }
 
 </script>
---
+++ src/components/SalaryForm.vue
@@ -0,0 +1,52 @@
+<template>
+
+    <v-card flat tile>
+      <v-text-field
+        :label="prompt"
+        type="number"
+        hide-details="auto"
+        @input="changeValue"
+      ></v-text-field>
+
+      <v-card-actions>
+        <v-spacer/>
+        <v-btn color="primary" @click="submitSalary">Confirm</v-btn>
+      </v-card-actions>
+    </v-card>
+</template>
+
+<script lang="ts">
+  import Vuetify from "vuetify";
+  import { Component, Vue } from "vue-property-decorator";
+
+
+  const SalaryFormProps = Vue.extend({
+    props: {
+      prompt: String,
+      eventId: String
+    }
+  });
+
+  @Component
+  export default class SalaryForm extends SalaryFormProps {
+    vuetify = new Vuetify();
+
+    salary = -1;
+
+    changeValue(newSalary:number):void {
+      this.salary = newSalary;
+    }
+
+    submitSalary():void {
+        if (this.salary > 0) {
+            this.$emit('setSalary'+ this.$props.eventId, this.salary);
+        } else {
+            console.log('this is too small.')
+        }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>

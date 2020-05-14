ada3b43c59d48f007c1f4ab12928cc59fee092cd
Nick Lapitsky <nick.lapitsky@cinarra.com>
2020-05-14 20:12:23 +0300

    Fix temperature test

--- tests/unit/ResultDialog.spec.ts
+++ tests/unit/ResultDialog.spec.ts
@@ -1,4 +1,4 @@
-import { mount,createLocalVue} from "@vue/test-utils";
+import { mount, createLocalVue} from "@vue/test-utils";
 import ResultDialog from "@/components/ResultDialog.vue";
 import Vue from 'vue'
 import Vuetify from "vuetify";
@@ -12,18 +12,17 @@ describe("SalaryNegotiation.vue", () => {
 
   let vuetify = new Vuetify();
   const temperature = 451;
-
+  const mockSuccessResponse = {main:{temp: temperature}};
+  const mockJsonPromise = Promise.resolve(mockSuccessResponse); // 2
+  const mockFetchPromise = Promise.resolve({ // 3
+    json: () => mockJsonPromise,
+  });
+  global.fetch = jest.fn().mockImplementation(() => mockFetchPromise);
 
   beforeEach(() => {
-    vuetify = new Vuetify()
+    vuetify = new Vuetify();
 
-    const mockSuccessResponse = {main:{temp: temperature}};
-    const mockJsonPromise = Promise.resolve(mockSuccessResponse); // 2
-    const mockFetchPromise = Promise.resolve({ // 3
-      json: () => mockJsonPromise,
-    });
-    global.fetch = jest.fn().mockImplementation(() => mockFetchPromise);
-  })
+  });
 
   it("renders result when shown", () => {
     const result = "This was a triumph.";
@@ -31,19 +30,26 @@ describe("SalaryNegotiation.vue", () => {
       localVue,
       vuetify,
       propsData: { result, isShown:true }
-    })
+    });
     expect(wrapper.text()).toMatch(result);
   });
 
 
-  it("shows right temperature", () => {
+  it("shows right temperature", async () => {
     const result = "It's hard to overstate my satisfaction.";
+
     const wrapper = mount(ResultDialog, {
       localVue,
       vuetify,
       propsData: { result, isShown:true }
-    })
-    expect(wrapper.text()).toMatch(temperature.toString());
+    });
+
+    await mockFetchPromise;
+    await wrapper.vm.$forceUpdate();
+
+    await wrapper.vm.$nextTick(() => {
+      expect(wrapper.text()).toMatch(temperature.toString());
+    });
   });
 });
 

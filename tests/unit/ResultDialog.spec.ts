93d7c83097bfb6d1968b46349f7cc9ab7b83be75
Nick Lapitsky <nick.lapitsky@cinarra.com>
2020-05-14 21:22:43 +0300

    Add other result dialog tests

--- tests/unit/ResultDialog.spec.ts
+++ tests/unit/ResultDialog.spec.ts
@@ -1,27 +1,33 @@
-import { mount, createLocalVue} from "@vue/test-utils";
+import { mount, createLocalVue } from "@vue/test-utils";
 import ResultDialog from "@/components/ResultDialog.vue";
-import Vue from 'vue'
+import Vue from "vue";
 import Vuetify from "vuetify";
 
 Vue.use(Vuetify);
 
-
 const localVue = createLocalVue();
 
 describe("SalaryNegotiation.vue", () => {
-
   let vuetify = new Vuetify();
   const temperature = 451;
-  const mockSuccessResponse = {main:{temp: temperature}};
-  const mockJsonPromise = Promise.resolve(mockSuccessResponse); // 2
-  const mockFetchPromise = Promise.resolve({ // 3
-    json: () => mockJsonPromise,
+  const mockSuccessResponse = { main: { temp: temperature } };
+  const mockJsonPromise = Promise.resolve(mockSuccessResponse);
+  const mockFetchPromise = Promise.resolve({
+    json: () => mockJsonPromise
   });
+
+  const mockRejectPromise = Promise.resolve(() =>
+    Promise.reject("We are closed for lunch")
+  );
   global.fetch = jest.fn().mockImplementation(() => mockFetchPromise);
 
   beforeEach(() => {
     vuetify = new Vuetify();
+  });
 
+  afterAll(() => {
+    global.fetch.mockClear();
+    delete global.fetch;
   });
 
   it("renders result when shown", () => {
@@ -29,11 +35,10 @@ describe("SalaryNegotiation.vue", () => {
     const wrapper = mount(ResultDialog, {
       localVue,
       vuetify,
-      propsData: { result, isShown:true }
+      propsData: { result, isShown: true }
     });
     expect(wrapper.text()).toMatch(result);
   });
-
 
   it("shows right temperature", async () => {
     const result = "It's hard to overstate my satisfaction.";
@@ -41,15 +46,45 @@ describe("SalaryNegotiation.vue", () => {
     const wrapper = mount(ResultDialog, {
       localVue,
       vuetify,
-      propsData: { result, isShown:true }
+      propsData: { result, isShown: true }
     });
 
     await mockFetchPromise;
     await wrapper.vm.$forceUpdate();
 
-    await wrapper.vm.$nextTick(() => {
-      expect(wrapper.text()).toMatch(temperature.toString());
+    expect(wrapper.text()).toMatch(temperature.toString());
+  });
+
+  it("handles temperature service errors", async () => {
+    const result = "It's hard to overstate my satisfaction.";
+
+    global.fetch = jest.fn().mockImplementation(() => mockRejectPromise);
+
+    const wrapper = mount(ResultDialog, {
+      localVue,
+      vuetify,
+      propsData: { result, isShown: true }
     });
+
+    await mockRejectPromise;
+
+    await wrapper.vm.$forceUpdate();
+    expect.assertions(1);
+
+    expect(wrapper.text()).toMatch("error loading temperature");
+  });
+
+  it("emits hide event", async () => {
+    const result = "It's hard to overstate my satisfaction.";
+
+    const wrapper = mount(ResultDialog, {
+      localVue,
+      vuetify,
+      propsData: { result, isShown: true }
+    });
+
+    wrapper.find("button").trigger("click");
+    await wrapper.vm.$nextTick();
+    expect(wrapper.emitted().hide).toBeTruthy();
   });
 });
-
